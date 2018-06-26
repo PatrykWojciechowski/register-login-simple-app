4854bb2d8b30566befb2a368d626ffb8d9756f50
PatrykWojciechowski <patrickwww94@gmail.com>
2018-06-26 11:55:07 +0200

    delete duplicated packages

--- src/main/java/com/wojciechowski/project/forum/repositories/RoleRepository.java
+++
@@ -1,9 +0,0 @@
-package com.wojciechowski.project.forum.repositories;
-
-import org.springframework.data.repository.CrudRepository;
-
-import com.wojciechowski.project.forum.domain.Role;
-
-public interface RoleRepository extends CrudRepository<Role, Long> {
-
-}
--- src/main/java/com/wojciechowski/project/forum/services/SecurityService.java
+++
@@ -1,9 +0,0 @@
-package com.wojciechowski.project.forum.services;
-
-public interface SecurityService {
-
-	String findLoggedInUsername();
-
-	void autologin(String username, String password);
-
-}
